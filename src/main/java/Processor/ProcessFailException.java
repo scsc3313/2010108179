203cb1b56e30925979639d03951e34d178c546f7
Dongha Kim <ghost9087@gmail.com>
2015-11-23 22:00:05 +0900

    reset project

--- src/main/java/Processor/ProcessFailException.java
+++
@@ -1,6 +0,0 @@
-package processor;
-
-/**
- * Created by ghost9087 on 2015. 11. 18..
- */
-public class ProcessFailException extends Exception {}
--- src/main/java/Processor/TypeNotMatchedException.java
+++
@@ -1,8 +0,0 @@
-package processor;
-
-import java.security.InvalidParameterException;
-
-/**
- * Created by ghost9087 on 2015. 11. 17..
- */
-public class TypeNotMatchedException extends InvalidParameterException {}
